b7e2f10f19fa36b2cd3efa65889747746ea685b2
Srihas Reddy Challa <challasrihasreddy@gmail.com>
2025-06-10 23:21:44 +0530

    Create Dashboard and add shadcn components

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+const page = () => {
+  return (
+    <div>
+        <h1 className="text-2xl font-bold">Dashboard</h1>
+        <p className="text-gray-500">This is the dashboard page.</p>
+        <p className="text-gray-500">You can access this page because you are authenticated.</p>
+    </div>
+  )
+}
+
+export default page
